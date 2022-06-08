c752f34eaed107e3918c4f504d31636513daf626
fjquen <fjquen@gmail.com>
2022-06-08 17:57:38 +0200

    bouton changement image

--- multigame/js/memory.js
+++ multigame/js/memory.js
@@ -1,36 +1,59 @@
+/*eslint-env browser*/
 function memory() {
-    var memoryAnimals = [{id:1, image:"https://mesjoursferies.com/wp-content/uploads/sites/8/2020/03/date-jour-ferie-fete-travail-1170x694.jpg",imageDefaut:"https://www.dcplanet.fr/wp-content/uploads/2021/12/og.jpg"},
-                        {id:2, image:"https://mesjoursferies.com/wp-content/uploads/sites/8/2020/03/date-jour-ferie-fete-travail-1170x694.jpg",imageDefaut:"https://www.dcplanet.fr/wp-content/uploads/2021/12/og.jpg"},
-                        {id:3, image:"https://mesjoursferies.com/wp-content/uploads/sites/8/2020/03/date-jour-ferie-fete-travail-1170x694.jpg",imageDefaut:"https://www.dcplanet.fr/wp-content/uploads/2021/12/og.jpg"},
-                        {id:4, image:"https://mesjoursferies.com/wp-content/uploads/sites/8/2020/03/date-jour-ferie-fete-travail-1170x694.jpg",imageDefaut:"https://www.dcplanet.fr/wp-content/uploads/2021/12/og.jpg"}]
-    
-    
+    var memoryAnimals = [{
+            id: "fruit",
+            image: "https://mesjoursferies.com/wp-content/uploads/sites/8/2020/03/date-jour-ferie-fete-travail-1170x694.jpg",
+            imageDefaut: "https://www.dcplanet.fr/wp-content/uploads/2021/12/og.jpg"
+        },
+        {
+            id: "fruit",
+            image: "https://mesjoursferies.com/wp-content/uploads/sites/8/2020/03/date-jour-ferie-fete-travail-1170x694.jpg",
+            imageDefaut: "https://www.dcplanet.fr/wp-content/uploads/2021/12/og.jpg"
+        },
+        {
+            id: "pizza",
+            image: "https://mesjoursferies.com/wp-content/uploads/sites/8/2020/03/date-jour-ferie-fete-travail-1170x694.jpg",
+            imageDefaut: "https://www.dcplanet.fr/wp-content/uploads/2021/12/og.jpg"
+        },
+        {
+            id: "pizza",
+            image: "https://mesjoursferies.com/wp-content/uploads/sites/8/2020/03/date-jour-ferie-fete-travail-1170x694.jpg",
+            imageDefaut: "https://www.dcplanet.fr/wp-content/uploads/2021/12/og.jpg"
+        }]
 
-   
+
+
+
     /* It's creating a button for each element in the array. It's creating a clone of the array. */
-    for (let index = 0; index < memoryAnimals.length; index++) {
-        const iDanimal = memoryAnimals[index].id
+    for (var index = 0; index < memoryAnimals.length; index++) {
         var img = document.createElement("img");
         img.src = memoryAnimals[index].imageDefaut;
-        img.style.width="100%"
+        img.style.width = "100%"
         var btn = document.createElement("BUTTON")
-        var textBtn = document.createTextNode(iDanimal)
+        var textBtn = document.createTextNode(index)
         btn.appendChild(textBtn)
         document.body.appendChild(btn)
         btn.append(img)
-        btn.id=iDanimal
-        btn.style.width="35%"
-        
-    }
-    
+        btn.id = memoryAnimals[index].id
+        btn.style.width = "35%"
 
-    let all_btn = document.querySelectorAll('button');
-    all_btn.forEach(function(btn) {
-            btn.addEventListener("click", function() {
-                    const idMemory = btn.getAttribute("id");
-                    console.log(idMemory)
-                
-            })
+    }
+
+
+
+
+    var all_btn = document.querySelectorAll('button');
+    all_btn.forEach(function (btn) {
+        btn.addEventListener("click", function () {
+            var idMemory = btn.getAttribute("id");
+            // eslint-disable-next-line no-console
+            console.log(idMemory)
+            // eslint-disable-next-line no-console
+            console.log(btn.textContent)
+            img.src = memoryAnimals[btn.textContent].image;
+            btn.append(img)
+        })
     });
+
 }
 memory()
