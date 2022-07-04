41659c96c3c32db7b66801600d1580692fc611c2
fjquen <fjquen@gmail.com>
2022-07-04 15:14:09 +0200

    timer javascript whacamole

--- multigame/js/whacamole.js
+++ multigame/js/whacamole.js
@@ -2,13 +2,23 @@ function whacamole() {
 
     var moleTab = [{
         name: "moleStar",
-        hole: false
+        hole: null
     }, {
         name: "molePingouin",
-        hole: false
+        hole: null
+    },{
+        name: "moleRyo",
+        hole: null
+    }, {
+        name: "molePascal",
+        hole: null
+    },{
+        name: "moleMéteor",
+        hole: null
+    }, {
+        name: "moleScorpion",
+        hole: null
     }]
-
-
 
 
       var elementPause = "pause"
@@ -18,23 +28,38 @@ function whacamole() {
       document.body.appendChild(btn)
       btn.id=elementPause
 
-
       var elementPlay = "play"
       var btn = document.createElement("BUTTON")
       var textBtn = document.createTextNode(elementPlay)
       btn.appendChild(textBtn)
       document.body.appendChild(btn)
       btn.id=elementPlay
+
+      for (var i = 0; i < moleTab.length; i++) {
+        var btnMole = document.createElement("BUTTON")
+        var textBtn = document.createTextNode("trou")
+        btnMole.appendChild(textBtn)
+        document.body.appendChild(btnMole)
+        btnMole.id = moleTab[i].name
+      }
+
       document.getElementById(elementPlay).addEventListener("click", function(){
         var moleInterval = setInterval(function() {  var numMole = Math.trunc(Math.random() * moleTab.length)
-        moleTab[numMole].hole = true
-        console.log(moleTab[numMole].name + " " + moleTab[numMole].hole)}, 1000)
 
-        console.log(moleInterval);
+          console.log(moleTab[numMole].name)
+          document.getElementById(moleTab[numMole].name).textContent = moleTab[numMole].name
+          var myTimeout = setTimeout(function(){
+            document.getElementById(moleTab[numMole].name).textContent = moleTab[numMole].hole
+          }, 5000);
+        }
+        , 1000)
 
         document.getElementById(elementPause).addEventListener("click", function(){
           clearInterval(moleInterval)
         });
+
+
+
       });
 }
 whacamole()
